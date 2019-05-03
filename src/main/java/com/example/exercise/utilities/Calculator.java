6f6d2402cc0fdb729a98237028a454995e1858ff
naga <naga@area-b.com>
2019-05-04 01:28:47 +0900

    可変長引数にて渡された値をすべて加算した結果を返却するメソッドを追加

--- src/main/java/com/example/exercise/utilities/Calculator.java
+++ src/main/java/com/example/exercise/utilities/Calculator.java
@@ -1,8 +1,14 @@
 package com.example.exercise.utilities;
 
+import java.util.Arrays;
+
 public class Calculator {
 
     public int add(int arg1, int arg2) {
         return arg1 + arg2;
+    }
+
+    public int add(int... args) {
+        return Arrays.stream(args).sum();
     }
 }
