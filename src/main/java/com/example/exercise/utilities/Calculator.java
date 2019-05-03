d806124743cfeb62348e73d50b686b0781b4b776
naga <naga@area-b.com>
2019-05-04 01:30:27 +0900

    引数を2つ取るメソッドが不要になったので削除

--- src/main/java/com/example/exercise/utilities/Calculator.java
+++ src/main/java/com/example/exercise/utilities/Calculator.java
@@ -4,10 +4,6 @@ import java.util.Arrays;
 
 public class Calculator {
 
-    public int add(int arg1, int arg2) {
-        return arg1 + arg2;
-    }
-
     public int add(int... args) {
         return Arrays.stream(args).sum();
     }
