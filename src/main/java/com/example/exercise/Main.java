556b7cd052d15fde9ffc0592f82a33e29a5b1a5a
naga <naga@area-b.com>
2019-05-04 01:48:35 +0900

    実行時引数を使って足し算するようにMainを変更。pomにjarファイルビルド用の設定を追加。

--- src/main/java/com/example/exercise/Main.java
+++ src/main/java/com/example/exercise/Main.java
@@ -2,10 +2,14 @@ package com.example.exercise;
 
 import com.example.exercise.utilities.Calculator;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
 
+        int[] numbers = Arrays.stream(args).mapToInt(Integer::valueOf).toArray();
+
         Calculator calculator = new Calculator();
-        System.out.println(calculator.add(1, 4));
+        System.out.println(calculator.add(numbers));
     }
 }
